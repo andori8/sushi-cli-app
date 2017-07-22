a99a2dc31d67dc5194e9f6628b1a8181cc66f01a
Ryan Do <falcsmike7@gmail.com>
2017-07-22 16:46:52 +0000

    defined cli methods

--- lib/sushi_usa/cli.rb
+++ lib/sushi_usa/cli.rb
@@ -1,5 +1,36 @@
 class SushiUsa::Cli
   def call
     puts "Here are the top 15 sushi restaurants in America as of 2017:"
+    list_restaurants
+    menu
+    goodbye
+  end
+
+  def list_restaurants
+    puts <<~DOC
+    1. Sushi Nakazawa - New York
+    2. Sushi on Jones - New York
+    DOC
+  end
+
+  def menu
+    input = nil
+
+    while input != "exit"
+    puts "Please choose restaurant (1-15) for more details or type 'list' or 'exit':"
+    input = gets.strip.downcase
+
+      if input.to_i.between?(1,15)
+        puts "restaurant details"
+      elsif input == "list"
+        list_restaurants
+      else
+        puts "Not a valid choice."
+      end
+    end
+  end
+
+  def goodbye
+    puts "Thank you and good bye!"
   end
 end
