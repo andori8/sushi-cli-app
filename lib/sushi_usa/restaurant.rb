a0a8b4346ef32ed7ef2888c3e347fc7f4ac73e98
Ryan Do <falcsmike7@gmail.com>
2017-07-22 19:50:14 +0000

    restaurant class initialize

--- lib/sushi_usa/restaurant.rb
+++ lib/sushi_usa/restaurant.rb
@@ -1,17 +1,13 @@
 class Restaurant
   attr_accessor :name, :city, :url
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    self.class.all << self
+  end
 
   def self.all
-    restaurant_1 = self.new
-    restaurant_1.name = "Sushi Nakazawa"
-    restaurant_1.city = "New York"
-    restaurant_1.url = "http://welovejapanesefood.com/10-best-sushi-restaurants-in-the-united-states/#nakazawa"
-
-    restaurant_2 = self.new
-    restaurant_2.name = "Sushi on Jones"
-    restaurant_2.city = "New York"
-    restaurant_2.url = "http://welovejapanesefood.com/10-best-sushi-restaurants-in-the-united-states/#jones"
-
-    [restaurant_1, restaurant_2]
+    @@all
   end
 end
